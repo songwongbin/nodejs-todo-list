57cecd2cfc63958a59fdbd41c0a6b9244827dec2
songwongbin <bb201802@naver.com>
2024-11-21 11:56:08 +0900

    ignore retry

--- middlewares/error-handler.middleware.js
+++
@@ -1,8 +0,0 @@
-export default (err, req, res, next) => {
-  console.error(err);
-
-  if (err.name === "ValidationError") {
-    return res.status(400).json({ errorMessage: err.message });
-  }
-  return res.status(500).json({ errorMessage: "서버 에러 발생!" });
-};
